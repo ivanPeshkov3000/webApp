b4c385d498331939e9b5c16eec5e7d17e5835a62
Vitaliy V <vitalik.voronov1989@ya.ru>
2022-05-08 13:17:34 +0300

    v0.0.1

---
+++ app.js
@@ -0,0 +1,32 @@
+class App {
+  constructor(telegramSpace = window.Telegram.WebApp, content = []) {
+    this.tg = telegramSpace || (() => {
+      throw new Error("Not load Telegram WebApp")
+    })()
+    this.content = content;
+    this.appContainer = document.getElementById("app") || (() => {
+      const container = document.createElement('div');
+      container.id = "app";
+      document.body.appendChild(container)
+      return container;
+    })()
+
+  };
+
+  resize(h) {
+    this.appContainer.style.height = h;
+  }
+
+  init() {
+    this.resize(tgApp.viewportStableHeight);
+    if (!this.content.length) {
+      return this.appContainer.innerHTML = "<h2>Is empty app</h2>"
+    }
+
+    const frag = new DocumentFragment();
+    for (const block in this.content) {
+      frag.appendChild(block);
+    }
+    this.appContainer.appendChild(frag);
+  }
+}
