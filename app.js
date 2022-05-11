60a1fb006412126e08ac7662ab98fb06de14f5a3
Vitaliy V <vitalik.voronov1989@ya.ru>
2022-05-11 11:04:27 +0300

    ref008 0.0.2

---
+++ app.js
@@ -0,0 +1,10 @@
+// let width = window.innerWidth; //получаем ширину экрана
+// let height = window.innerHeight; // получаем высоту экрана
+// const color = '#fbafcc';
+
+// const app = new PIXI.Application(width, height); //создаем холст
+// document.body.appendChild(app.view); //выводим его в тело страницы
+
+// const pixel = new PIXI.Graphics(); //создаем новый графический элемент
+// pixel.lineStyle(0); //начинаем рисовать
+// pixel.beginFill(color, 1); //задаем цвет
