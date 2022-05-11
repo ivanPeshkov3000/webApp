f6a88343e8749b1bf0de2a290aa19479c3bdf6b9
Vitaliy V <vitalik.voronov1989@ya.ru>
2022-05-12 01:41:11 +0300

    ref023 0.0.2

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+let randcolRect;
 function tetrisRun() {
   let width = window.outerWidth; //получаем ширину экрана
   let height = window.Telegram.WebApp.viewportStableHeight; // получаем высоту экрана
-  const color = 0xABACCC;
+  const color = 0xABACD0;
 
   const app = new PIXI.Application({ width, height, backgroundColor: 0xF1F1C2
 }); //создаем холст
@@ -17,11 +18,72 @@ function tetrisRun() {
 
   pixel.interactive = true; //делаем интерактивным
   pixel.buttonMode = true; //меняем курсор при наведении
+
   app.stage.addChild(pixel); //выводим на холсте
 
+  randcolRect = () => {
+    pixel.beginFill(randomColor(), 1)
+    pixel.endFill();
+  }
 
-  const gravity = 2;
-  app.ticker.add(() => {
+  const gravity = 0.5;
+  app.ticker.add(function getDown() {
+    if ((pixel.x + pixel.height/2) > app.height) {
+      app.ticker.remove(getDown);
+    }
     pixel.y += gravity;
   });
+}
+
+function randomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '0x';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+
+// Коллизия точек
+function pointCollision(point1, point2) {
+  return (point1.x === poin2.x && point1.y === point2.y) ? true : false;
+}
+
+// Коллизия кругов
+function circleCollision(circle1, circle2) {
+  let dx = Math.abs(circle1.x - circle2.x);
+  let dy = Math.abs(circle1.y - circle2.y);
+  let dr = circle1.radius + circle2.radius;
+  
+  return (Math.sqrt(dx^2 + dy^2) < dr) ? true : false;
+}
+
+// Коллизия круга и точки
+function circpointCollision(circle, point) {
+  let dx = Math.abs(circle.x - point.x);
+  let dy = Math.abs(circle.y - point.y);
+  let dr = circle.radius;
+
+  return (Math.sqrt(dx ^ 2 + dy ^ 2) < dr) ? true : false;
+}
+
+// Коллизия квадратов
+function rectCollision(rect1, rect2) {
+  let dx = rect1.x - rect2.x; // Расстояние между центрами по X
+  let dy = rect1.y - rect2.y; // Расстояние между центрами по Y
+  let rx = (rect1.width + rect2.width) / 2; // Минимальноe расстояние по X
+  let ry = (rect1.height + rect2.height) / 2; // Минимальное расстояние по Y
+
+  return (Math.abs(dx) < rx && Math.abs(dy) < ry) ? true : false;
+}
+
+// Коллизия квадрата и точки
+function rectpointCollision(rect, point) {
+  let dx = rect.x - point.x; // Расстояние между центрами по X
+  let dy = rect.y - point.y; // Расстояние между центрами по Y
+  let rx = rect.width / 2; // Минимальноe расстояние по X
+  let ry = rect.height / 2; // Минимальное расстояние по Y
+
+  return (Math.abs(dx) < rx && Math.abs(dy) < ry) ? true : false;
 }
